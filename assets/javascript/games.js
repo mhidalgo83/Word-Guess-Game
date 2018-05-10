f0f40b36b68de1877df880352587ace992f25658
Matt <makeshifthidalgo@hotmail.com>
2018-05-10 14:30:08 -0700

    letters and spaces are on the screen now

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -1,38 +1,122 @@
- 
- // The player starts with 10 guesses...
-var userGuess = 10;
+// VARIABLES
 
-// Words for our hangman game.
-var wordChoice = ["radagast", "frodo", "legolas", "gimli", "gandalf", "merry", "samwise", "rivendell", "elrond", "aragorn",
-    "pippin", "eowyn", "gollum", "boromir", "faramir"];
+//===========================================================
 
-// Function for choosing our random word from list above.
-function randomWord () {
-    wordChoice[Math.floor(Math.random() * wordChoice.length)];
-}
+// Words for our hangman game...
+var wordChoice = ["dubbel", "stout", "porter", "gose", "hefeweizen"];
 
-// Array for letters in randomly selected word.
+// Word chosen by computer...
+var word = wordChoice[Math.floor(Math.random() * wordChoice.length)];
+
+// Used letters...
+var usedLetters = [];
+
+// Used words...
+var usedWords = [];
+
+// Array for letters in randomly selected word...
 var answerArray = [];
-for (var i = 0; i < randomWord.length; i++) {
+for (var i = 0; i < word.length; i++) {
     answerArray[i] = "_";
 }
+// Guess left...
+var guessesLeft = 10;
+
+// Remaining letters to be guessed
+var remainingLetters = word.length;
+
+// FUNCTIONS
+
+//============================================================
+
+// This function chooses our word...
+function randomWord() {
+    return wordChoice[Math.floor(Math.random() * wordChoice.length)];
+}
+// This displays our word as underscored blank spaces...
+document.getElementById("word").innerHTML = answerArray.join(" ");
+
+//This function takes a letter the user pressed...
+document.onkeyup = function guess(event) {
+    // Stores it in the variable, switches to lower case...
+    var userGuess = event.key.toLowerCase();
+    // Puts the letter in our usedLetters array...
+    usedLetters.push(userGuess);
+    // And displays the used letters on the page...
+    document.getElementById("guesses").innerHTML = usedLetters;
+}
+
+// Take the userGuess, compare it to the answerArray, and see if the letter matches...   should I use indexOf or charAt??
+
+    // put letter into answerArray
+    // ???
+    // else 
+    // reduce guesseLeft --1;
+
+// }
+
+// Guessed letter
+
+
+
+
+
+
+// Computer choice...
+
+
+
+
+
+
+// Display remaining letters to be guessed using length of word
+
+
+
+
+
+
+
+
+
+
+// document.getElementById("#word").innerHTML = word;
+
+// The player starts with 10 guesses...
+// var userGuess = 10;
+
+
 
 // Determines how many letters are left in our word.
-var remainingLetters = randomWord.length;
+// var remainingLetters = randomWord.length;
+
+
 
 //
-while (remainingLetters > 0) {
-    event.onkeypress //any letter//{
-        if //Char === letter in word, run function wins//
-        else //subtract 1 from userGuess total{
-            if //userGuess = 0, end game
-            else //continue game
-        }
-    }
-    }
-    //Game code
-    //Take input from player
-    //Track used letters
-    //
-    //If true, update answerArray and remainingLetters for every correct guess, else decrease userGuess counter
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Function to update userGuess if guess is incorrect...
+// function updateGuesses () {
+//     document.getElementById("#guesses").innerHTML = "Guesses left:" + userGuess;
+// };
+
+
+
+
+// Function to add underscored spaces in place of characters in random word...
