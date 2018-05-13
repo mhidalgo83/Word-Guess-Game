207a16d1c883779a38445199409112b0b91f9d38
Matt <makeshifthidalgo@hotmail.com>
2018-05-12 17:03:54 -0700

    finished game

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -1,98 +1,88 @@
-// VARIABLES
-
-//===========================================================
 
 // Words for our hangman game...
-var wordChoice = ["dubbel", "stout", "porter", "gose", "hefeweizen"];
+var wordChoice = ["dubbel", "stout", "porter", "gose", "hefeweizen", "tripel", "schwarzbier", "amber", "blonde", "bock"];
+
+// Function that chooses random word
+function randomWord() {
+    return wordChoice[Math.floor(Math.random() * wordChoice.length)];
+}
 
 // Word chosen by computer...
-var word = wordChoice[Math.floor(Math.random() * wordChoice.length)];
+var word = randomWord();
+
 
 // Used letters...
 var usedLetters = [];
-
-// Used words...
-var usedWords = [];
+var answerArray;
 
 // Array for letters in randomly selected word...
-var answerArray = [];
-for (var i = 0; i < word.length; i++) {
-    answerArray[i] = "_";
+function answer() {
+    answerArray = [];
+    for (var i = 0; i < word.length; i++) {
+        answerArray[i] = "_";
+    }
 }
+
+answer();
+
 // Guess left...
 var guessesLeft = 10;
 
 // Remaining letters to be guessed
 var remainingLetters = word.length;
 
-// FUNCTIONS
-
-//============================================================
-
-// This function chooses our word...
-function randomWord() {
-    return wordChoice[Math.floor(Math.random() * wordChoice.length)];
-}
-// This displays our word as underscored blank spaces...
-document.getElementById("word").innerHTML = answerArray.join(" ");
 
 //This function takes a letter the user pressed...
-document.onkeyup = function guess(event) {
+console.log(word)
+document.onkeyup = function (event) {
     // Stores it in the variable, switches to lower case...
     var userGuess = event.key.toLowerCase();
+    document.getElementById("you-win").innerHTML = " ";
+    document.getElementById("you-lose").innerHTML = " ";
     // Puts the letter in our usedLetters array...
     usedLetters.push(userGuess);
+
     // And displays the used letters on the page...
+    if (word.indexOf(userGuess) == -1) {
+        guessesLeft--;
+    }
+    else {
+        for (var i = 0; i < word.length; i++) {
+            if (userGuess == word.charAt(i)) {
+                answerArray[i] = userGuess;
+                remainingLetters--;
+                console.log(remainingLetters);
+            }
+
+        }
+    }
+
+    if (remainingLetters == 0) {
+        document.getElementById("you-win").innerHTML = "You win! Press any key to beer me!"
+        word = randomWord();
+        answer();
+        console.log(word)
+        guessesLeft = 10;
+        usedLetters = [];
+        remainingLetters = word.length;
+    }
+
+    if (guessesLeft == 0) {
+        document.getElementById("you-lose").innerHTML = "You lost! Press any key to chug another!"
+        word = randomWord();
+        answer();
+        console.log(word)
+        guessesLeft = 10;
+        usedLetters = [];
+        remainingLetters = word.length;
+    }
+
+
+    document.getElementById("word").innerHTML = answerArray.join(" ");
     document.getElementById("guesses").innerHTML = usedLetters;
+    document.getElementById("guesses-left").innerHTML = guessesLeft;
 }
 
-// Take the userGuess, compare it to the answerArray, and see if the letter matches...   should I use indexOf or charAt??
-
-    // put letter into answerArray
-    // ???
-    // else 
-    // reduce guesseLeft --1;
-
-// }
-
-// Guessed letter
-
-
-
-
-
-
-// Computer choice...
-
-
-
-
-
-
-// Display remaining letters to be guessed using length of word
-
-
-
-
-
-
-
-
-
-
-// document.getElementById("#word").innerHTML = word;
-
-// The player starts with 10 guesses...
-// var userGuess = 10;
-
-
-
-// Determines how many letters are left in our word.
-// var remainingLetters = randomWord.length;
-
-
-
-//
 
 
 
@@ -111,12 +101,13 @@ document.onkeyup = function guess(event) {
 
 
 
-// Function to update userGuess if guess is incorrect...
-// function updateGuesses () {
-//     document.getElementById("#guesses").innerHTML = "Guesses left:" + userGuess;
-// };
 
 
 
 
-// Function to add underscored spaces in place of characters in random word...
+
+
+
+
+
+
